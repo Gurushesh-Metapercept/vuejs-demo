c2b40465da1babb4a670391fc1bb586be8769ac2
Gurushesh <gurushesh.p@metapercept.com>
2023-02-01 16:16:41 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <span>VueJS</span>
+        <menu>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </menu>
+    </header>
+</template>
+<script>
+export default {
+    name : 'NavBar',
+}
+</script>
+
+<style scoped>
+span {
+    font-weight: bold;
+    font-size: 25px;
+}
+header {
+    background: rgb(52, 158, 207);
+    padding: 10px 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+
+}
+li {
+    list-style-type: none;
+
+}
+a {
+    text-decoration: none;
+    color: inherit;
+}
+menu ul{
+    display: flex;
+
+}
+
+menu li + li {
+    margin-left: 1rem;
+}
+</style>

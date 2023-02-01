386344fd3e12f8f63136e1364d388d9dcd8c72ef
Gurushesh <gurushesh.p@metapercept.com>
2023-02-01 18:15:01 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <Navbar/>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home data="Heleoooo" msg="hiii"/>
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue';
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    HelloWorld
+    Home
   }
 }
 </script>

92fc3785d95f14728d7c0041665022da42b4943e
Gurushesh <gurushesh.p@metapercept.com>
2023-02-02 16:57:53 +0530

    initial commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,8 @@
         <span>VueJS</span>
         <menu>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
             </ul>
         </menu>
     </header>
